3be7575920fe92d78833ce59df7a106add8797ff
Marwan-G <ghabin2004@gmail.com>
2017-08-13 00:21:22 +0200

    creatin custom directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  oddnumbers = [ 1, 3, 5, 7];
+  evennumbers = [2, 4, 8];
+   onlyodd = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BasicHighlightDirective} from './bacichighlight/basic-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule
